d166490b2484b28510e1fa9f15d224d1e51c6012
unknown <omar.er3434@gmail.com>
2025-05-14 19:59:08 +0300

    Add Check IsBooked in Single Event

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -82,9 +82,27 @@ exports.getAllEvents = async (req, res) => {
 exports.getEventById = async (req, res) => {
 
     try {
+
         const event = await Event.findById(req.params.id);
         if (!event) return res.status(404).json({ error: 'Event not found' });
-        res.json(event);
+
+        let isBooked = false;
+
+        if (req.user) {
+            const booking = await Booking.findOne({
+                user: req.user.id,
+                event: event._id
+            });
+            if (booking) {
+                isBooked = true;
+            }
+        }
+
+        res.json({
+            ...event.toObject(),
+            isBooked
+        });
+
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
